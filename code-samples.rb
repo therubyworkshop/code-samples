f517fbc31ab2a4a2f709c7ccf4519e25539e34fb
bglusman <brian@enjyn.com>
2012-04-27 16:54:13 -0400

    pastes from slides

--- code-samples.rb
+++ code-samples.rb
@@ -10,3 +10,98 @@ puts 3 * 2 # 6
 puts 9 / 2 # 4
 
 puts 5 * (12-8) + 15 # 35
+
+#########
+
+puts "hello"
+puts ""
+puts "Ruby Rocks!!!" 
+
+#########
+
+puts "hello" + " world"
+
+puts "welcome" * 6
+
+########
+
+#both of these give error
+'6' + 6  or '6' * 6
+
+#######
+
+puts 'You\'re awesome for showing up'
+
+puts "you're"
+puts '"Say what"'
+
+#######
+
+
+puts "There are #{60*60*24*365} seconds in a year"
+
+#######
+
+
+name = "Jearvon"
+greeting = 'hello' + name
+
+
+
+#######
+
+puts 'You\'re awesome for showing up'
+
+puts "you're"
+puts '"Say what"'
+
+#######
+
+
+puts "There are #{60*60*24*365} seconds in a year"
+
+#######
+
+
+name = "Jearvon"
+greeting = 'hello' + name
+
+######
+
+var1 = 5
+var2 = 1
+
+var1 = 'two'
+var2 = var1
+
+puts var2
+var1 = 'three'
+puts var2
+
+######
+
+name = gets.chomp
+name == "Jearvon"
+name.length > 5
+
+######
+
+if name == "Jearvon"
+  puts "Jearvon"
+else
+  puts "Not Jearvon"
+end 
+
+######
+
+while name.length < 100
+  name = "Sir " + name
+end
+
+######
+
+
+
+
+
+
