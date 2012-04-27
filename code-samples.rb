98d64f1a15ad2b2f57f01a033c407e86660b0d6b
Jearvon Dharrie <j.dharrie@gmail.com>
2012-04-27 15:30:17 -0400

    Added numbers examples

--- code-samples.rb
+++ code-samples.rb
@@ -1,1 +1,12 @@
 puts "Welcome to The Ruby Workshop"
+
+puts "NUMBERS"
+
+puts 3.0 + 3 # 6.0
+puts 4 + 4 # 8
+puts 2 - 1 # 1
+puts 6 / 2 # 3
+puts 3 * 2 # 6
+puts 9 / 2 # 4
+
+puts 5 * (12-8) + 15 # 35
